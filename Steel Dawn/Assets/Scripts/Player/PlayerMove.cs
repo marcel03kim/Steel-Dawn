3bf954e40bbda5f96ca5548b57b9124441390722
marcel03kim <marcel03kim@gmail.com>
2024-09-08 21:58:31 +0900

    feat : 플레이어 움직임 구현

---
+++ Steel Dawn/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float speed;
+    Vector2 movement = new Vector2();
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        rb.velocity = movement * speed;
+    }
+}
